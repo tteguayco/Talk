d0a927e63f36bd0494991e4e5eec5d7c4fe2063f
tteguayco <teguayco95@gmail.com>
2017-07-15 21:09:13 +0100

    Implementing class Socket (Socket() and ~Socket())

--- socket.cpp
+++ socket.cpp
@@ -2,10 +2,24 @@
 
 Socket::Socket(const sockaddr_in &address)
 {
+    // Create socket
+    fd_ = socket(domain, type, protocol);
 
+    // Assign socket to address
+    bind(fd_, (const sockaddr*) &address, sizeof(address));
 }
 
 Socket::~Socket()
 {
+    close(fd_);
+}
 
+bool Socket::socket_valid()
+{
+    if (fd_ >= 0)
+    {
+        return true;
+    }
+
+    return false;
 }
--- socket.h
+++ socket.h
@@ -1,8 +1,18 @@
 #ifndef SOCKET_H
 #define SOCKET_H
 
+#include <iostream>
 #include <sys/socket.h>
 #include <netinet/in.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+
+#define domain AF_INET
+#define type SOCK_DGRAM
+#define protocol 0
+
+#define SOCKET_CREATION_FAILED 2
+#define BIND_FAILED 3
 
 class Socket
 {
@@ -12,6 +22,7 @@ private:
 public:
     Socket(const sockaddr_in& address);
     ~Socket();
+    bool socket_valid();
 };
 
 #endif // SOCKET_H
